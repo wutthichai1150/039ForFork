fd445d49962035bf96bef83d68b69cb8bbcb9be0
JETSADA05 <112895243+JETSADA05@users.noreply.github.com>
2025-01-15 09:28:06 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
             <td width="23" rowspan="2">&nbsp;</td>
             <td height="119" colspan="2" bgcolor="#006600">
                 <h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนต์</font></center></h1>
-                <?php include "Date.php"; ?>
+                <?php include "Date.php"; ?>vvvvvv
             </td>
             <td width="43" rowspan="2">&nbsp;</td>
         </tr>
